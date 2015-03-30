f8fbe25f50d5d57c0fc4e6e2c657bc4e41d2beaa
Saul Hernandez <sauldhernandez@gmail.com>
2015-03-30 16:26:23 -0430

    test

--- src/sgb-datasource-json.js
+++ src/sgb-datasource-json.js
@@ -4,6 +4,7 @@ angular.module('sgb-datasource-json', ['megazord'])
     .factory('sgb-datasource-json', ['$http', '$translate', function ($http){
         return {
             getData: function(params) {
+                console.log('I GOT CALLED');
                 return $http.get(params.path)
                     .then(function(result){
                         return result.data;
